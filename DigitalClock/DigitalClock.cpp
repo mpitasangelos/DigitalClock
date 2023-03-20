37743c91a990d755335157c26b06a334fdd0bec0
abitas <abitas@EEFT-DS71W93>
2023-03-20 15:08:27 +0200

    Add project files.

---
+++ DigitalClock/DigitalClock.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+	int h, m, s;
+	int d = 1000;
+	printf("Set time : \n");
+	scanf_s("%d%d%d",&h,&m,&s);
+	if (h > 12 || m > 60 || s > 60) {
+		printf("ERROR ! \n");
+		exit(0);
+	}
+	while (1)
+	{
+		s++;
+		if (s > 59) {
+			m++;
+			s = 0;
+
+		}
+		if (m > 59) {
+			h++;
+			m = 0;
+		}
+		if (h > 12) {
+			h = 1;
+		}
+		printf("\n Clock :");
+		printf("\n %02d:%02d:%02d", h, m, s); //write time in 00:00:00 format
+		Sleep(d);
+		system("cls");
+	} 
+}
